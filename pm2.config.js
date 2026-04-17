828d78582fdaebd418b134880286ac6965177a45
Ulugbek Erkinov <ulugbek9015y@gmail.com>
2026-04-17 16:52:24 +0500

    add pm2.config.js for production deploy

---
+++ pm2.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  apps: [
+    {
+      name: "face-id-front",
+      script: "npx",
+      args: "serve -s dist -l 3000",
+      cwd: "/var/www/workers/face_data_admin_front",
+      env: {
+        NODE_ENV: "production",
+      },
+      instances: 1,
+      autorestart: true,
+      watch: false,
+      max_memory_restart: "200M",
+    },
+  ],
+};
